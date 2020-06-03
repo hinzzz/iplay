ac993208a8000cbb2a1f399989e731178cb0e5b3
hinzzz <157957329@qq.com>
2020-06-03 17:42:38 +0800

    product-数据库配置

--- iplay-product/src/test/java/com/hinz/iplay/product/IplayProductApplicationTests.java
+++ iplay-product/src/test/java/com/hinz/iplay/product/IplayProductApplicationTests.java
@@ -1,5 +1,6 @@
 package com.hinz.iplay.product;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.hinz.iplay.product.entity.BrandEntity;
 import com.hinz.iplay.product.service.BrandService;
 import org.junit.jupiter.api.Test;
@@ -19,19 +20,19 @@ class IplayProductApplicationTests {
         BrandEntity brandEntity = new BrandEntity();
 
         // 更新
-//        brandEntity.setBrandId(5L);
-//        brandEntity.setDescript("华为牛逼");
-//        brandService.updateById(brandEntity);
+        brandEntity.setBrandId(5L);
+        brandEntity.setDescript("华为牛逼");
+        brandService.updateById(brandEntity);
 
          //插入
-        brandEntity.setName("hinz");
-        brandService.save(brandEntity);
-        System.out.println("保存成功...");
+//        brandEntity.setName("hinz");
+//        brandService.save(brandEntity);
+//        System.out.println("保存成功...");
 
-        /*List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id",5L));
+        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id",2L));
         list.forEach((item)-> {
             System.out.println(item);
-        });*/
+        });
     }
 
 }
